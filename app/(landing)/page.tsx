c40617d48ddf0d6e11081fd2bfd59bfcbd37960e
Lyubomyr Kovbel <shpintz@Lyubomyrs-MacBook-Pro.local>
2023-12-04 20:44:03 +0200

    come on

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,13 +1,11 @@
-import { signInMessageVerification } from "../../lib/web3";
+
 import LandingPageHeader from "@/components/LandingPageHeader";
 import LandingHero from "@/components/LandingHero";
 import LandingFeatures from "@/components/LandingFeatures";
 import MintingFeature from "@/components/MintingFeature";
 
 export default function Home() {
-  const handleMessageSig = async () => {
-    await signInMessageVerification();
-  };
+
 
   return (
     <main className="bg-black w-[100%] h-[100%] text-white">
