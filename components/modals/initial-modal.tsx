6d20aff306cd0bfd43a2e5f8b67e8f853a4f20f4
Lyubomyr Kovbel <shpintz@Lyubomyrs-MacBook-Pro.local>
2023-12-06 18:54:22 +0200

    nothing feels good

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,9 +1,123 @@
 "use client"
 
+import { useEffect, useState } from "react";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "../ui/dialog"
+import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form"
+import { Input } from "../ui/input"
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import * as z from "zod";
+
+const formSchema = z.object({
+    name: z.string().min(1, {
+      message: "group name required",
+    }),
+    imageUrl: z.string().min(1, {
+      message: "group image required too",
+    }),
+  });
+
 export const InitialModal = () => {
+    const [isMounted, setIsMounted] = useState(false);
+    const router = useRouter();
+  
+    useEffect(() => {
+      setIsMounted(true);
+    }, []);
+  
+    const form = useForm({
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+        name: "",
+        imageUrl: "",
+      },
+    });
+  
+    const isLoading = form.formState.isSubmitting;
+  
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+      try {
+        await axios.post("/api/servers", values);
+        form.reset();
+        router.refresh();
+        window.location.reload();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
+    if (!isMounted) {
+      return null;
+    }
     return (
-        <div>
-            This is a modal
-        </div>
+        <Dialog open>
+        <DialogContent className="bg-white text-black p-0 overflow-hidden">
+          <DialogHeader className="pt-8 px-6">
+            <DialogTitle className="text-2xl text-center font-bold">
+              Customize your server
+            </DialogTitle>
+            <DialogDescription className="text-center text-zine-500">
+              Give your group a purpose
+            </DialogDescription>
+          </DialogHeader>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <div className="space-y-8 px-6">
+                <div className="flex items-center justify-center text-center">
+                  <FormField
+                    control={form.control}
+                    name="imageUrl"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+
+                          <FileUpload
+                            endpoint="serverImage"
+                            value={field.value}
+                            onChange={field.onChange}
+                          />
+
+                          <div>
+
+                          </div>
+
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                </div>
+  
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                        Server Name
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          disabled={isLoading}
+                          className="bg-zinc-300/50 border-0 focus-visable:ring-0 text-black focus-visable:rin-offset-0"
+                          placeholder="enter server name"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <DialogFooter className="bg-gray-200 px-6 py-4">
+                <Button disabled={isLoading} variant="secondary">
+                  Create
+                </Button>
+              </DialogFooter>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
     )
 }
